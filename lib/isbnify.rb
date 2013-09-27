35ae0961573d8307be1f95d2e882bd6994bb3cad
cseydel <christoph.seydel@absolventa.de>
2013-09-27 17:42:25 +0200

    add error handling to Isbnify class methods

--- lib/isbnify.rb
+++ lib/isbnify.rb
@@ -1,5 +1,39 @@
 require "isbnify/version"
 
 module Isbnify
-  # Your code goes here...
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def hyphinate_isbn(isbn_string = nil)
+      string_argument_validations(isbn_string)
+    end
+
+    def valid_isbn?(isbn_string = nil)
+      string_argument_validations(isbn_string)
+    end
+
+    def create_isbn(number = nil)
+      integer_argument_validations(number)
+    end
+
+    def string_argument_validations(isbn_string)
+      validate_presence_of_attribute(isbn_string)
+      validate_type_of_attribute(isbn_string, "String")
+    end
+
+    def integer_argument_validations(number)
+      validate_presence_of_attribute(number)
+      validate_type_of_attribute(number, "Integer")
+    end
+
+    def validate_presence_of_attribute(attribute)
+      raise ArgumentError, "expected argument not to be nil" if attribute.nil?
+    end
+
+    def validate_type_of_attribute(attribute, type)
+      raise ArgumentError, "expected argument to be #{type}" if not attribute.is_a?(const_get(type))
+    end
+  end
 end
--- spec/isbnify_spec.rb
+++ spec/isbnify_spec.rb
@@ -1,5 +1,60 @@
 require "spec_helper"
 
+class Test
+  include Isbnify
+end
+
 describe Isbnify do
-  it {pending}
+  describe "with correct includes" do
+    it "responds to hyphinate_isbn" do
+      Test.should respond_to(:hyphinate_isbn)
+    end
+
+    it "responds to valid_isbn?" do
+      Test.should respond_to(:valid_isbn?)
+    end
+
+    it "responds to valid_isbn?" do
+      Test.should respond_to(:create_isbn)
+    end
+  end
+
+  describe "with class methods" do
+    context "with string_argument_validations" do
+      it "raises ArgumentError on missing isbn_string" do
+        expect { Test.hyphinate_isbn }.to raise_error(ArgumentError)
+      end
+
+      it "raises ArgumentError on missing isbn_string with message" do
+        expect { Test.valid_isbn? }.to raise_error(ArgumentError, "expected argument not to be nil")
+      end
+
+      it "raises ArgumentError on not string object" do
+        expect { Test.hyphinate_isbn(1) }.to raise_error(ArgumentError)
+      end
+
+      it "raises ArgumentError on not string object with message" do
+        expect { Test.valid_isbn?(1) }.to raise_error(ArgumentError, "expected argument to be String")
+      end
+    end
+
+    context "with integer_argument_validations" do
+      it "raises ArgumentError on missing number" do
+        expect { Test.create_isbn }.to raise_error(ArgumentError)
+      end
+
+      it "raises ArgumentError on missing number with message" do
+        expect { Test.create_isbn }.to raise_error(ArgumentError, "expected argument not to be nil")
+      end
+
+      it "raises ArgumentError on not Integer number" do
+        expect { Test.create_isbn("error") }.to raise_error(ArgumentError)
+      end
+
+      it "raises ArgumentError on not Integer number with message" do
+        expect { Test.create_isbn("error") }.to raise_error(ArgumentError, "expected argument to be Integer")
+      end
+    end
+  end
+
 end
