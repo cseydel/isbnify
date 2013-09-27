be3c3f1df1b6d5b4362c23fd25fccf19e20aa1c5
cseydel <christoph.seydel@absolventa.de>
2013-09-27 13:47:41 +0200

    add rspec test to isbnify

---
+++ spec/isbnify_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe Isbnify do
+  it "just works." do
+    true.should eq true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'isbnify'
+
+RSpec.configure do |config|
+end
